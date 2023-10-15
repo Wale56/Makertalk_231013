7e0123bb4df32545bffed9b0163c61afc8e1fccf
Walter <gantenbeinw@bluewin.ch>
2023-10-15 16:10:19 +0200

    ChatGPT Antworten als Kommentar ergänzt

--- 3_Bealsche_Vermutung/bealsche_vermutung.py
+++ 3_Bealsche_Vermutung/bealsche_vermutung.py
@@ -98,14 +98,13 @@
 # ChatGPT
 # Sure, here's the Python version of the given MATLAB code for testing the Beal Conjecture:
 
-import math
 
+import math
 
 def gcd(a, b):
     while b != 0:
         a, b = b, a % b
     return a
-
 
 def testBealConjecture(A_range, B_range, C_range, x_range, y_range, z_range):
     for A in A_range:
@@ -121,13 +120,11 @@ def testBealConjecture(A_range, B_range, C_range, x_range, y_range, z_range):
                                 if sum_powers == C_power:
                                     print(f"Conjecture holds for A={A}, B={B}, C={C}, x={x}, y={y}, z={z}")
 
-
 def generateFibonacciNumbers(n):
     fibonacci_numbers = [0, 1]
     for i in range(2, n):
-        fibonacci_numbers.append(fibonacci_numbers[i - 1] + fibonacci_numbers[i - 2])
+        fibonacci_numbers.append(fibonacci_numbers[i-1] + fibonacci_numbers[i-2])
     return fibonacci_numbers
-
 
 # Define the range of values for A, B, C, x, y, and z
 A_range = range(1, 16)
@@ -142,7 +139,6 @@ print('Testing Small Values of A, B, and C:')
 testBealConjecture(A_range, B_range, C_range, x_range, y_range, z_range)
 
 # Test 2: Large Values of A, B, and C
-# ----> Wegen langer Laufzeit ausgeblendet
 # print('Testing Large Values of A, B, and C:')
 # A_range_large = range(10**8, 10**9 + 1)
 # B_range_large = range(10**8, 10**9 + 1)
@@ -153,14 +149,12 @@ testBealConjecture(A_range, B_range, C_range, x_range, y_range, z_range)
 print('Testing Primes and Prime Powers:')
 primes_range = list(filter(lambda x: all(x % d != 0 for d in range(2, int(math.sqrt(x)) + 1)), range(2, 101)))
 primes_powers_range = range(3, 10)
-testBealConjecture(primes_range, primes_range, primes_range, primes_powers_range, primes_powers_range,
-                   primes_powers_range)
+testBealConjecture(primes_range, primes_range, primes_range, primes_powers_range, primes_powers_range, primes_powers_range)
 
 # Test 4: Consecutive Numbers
 print('Testing Consecutive Numbers:')
 consecutive_range = range(1, 11)
-testBealConjecture(consecutive_range, [i + 1 for i in consecutive_range], [i + 2 for i in consecutive_range], x_range,
-                   y_range, z_range)
+testBealConjecture(consecutive_range, [i+1 for i in consecutive_range], [i+2 for i in consecutive_range], x_range, y_range, z_range)
 
 # Test 5: Fibonacci Numbers
 print('Testing Fibonacci Numbers:')
