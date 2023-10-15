b5f4c718300761f419b09c34669db0987f634ad1
Walter <gantenbeinw@bluewin.ch>
2023-10-15 15:37:28 +0200

    ChatGPT Antworten als Kommentar ergänzt

--- 3_Bealsche_Vermutung/bealsche_vermutung.py
+++ 3_Bealsche_Vermutung/bealsche_vermutung.py
@@ -1,9 +1,111 @@
+# -> Bitte nach Python portieren:
+# %% some background
+# % The Beal Conjecture is a mathematical "hypothesis" proposed by businessman
+# % Andrew Beal in 1993. It posits that for any three positive integers A, B,
+# % and C, and any positive integers x, y, and z (greater than 2), if A^x +
+# % B^y = C^z, then A, B, and C must have a common factor greater than 1. The
+# % conjecture remains unproven, and a counterexample has not been found to
+# % disprove it, making it one of the unsolved problems in number theory.
+# % Andrew Beal established a $1 million prize for anyone who can provide a
+# % valid proof or disprove the conjecture.
+#
+# %%
+#     % Define the range of values for A, B, C, x, y, and z
+#     A_range = 1:15;    % Modify the range as needed
+#     B_range = 1:15;    % Modify the range as needed
+#     C_range = 1:15;    % Modify the range as needed
+#     x_range = 3:11;     % Modify the range as needed
+#     y_range = 3:11;     % Modify the range as needed
+#     z_range = 3:11;     % Modify the range as needed
+#
+#     % Test 1: Small Values of A, B, and C
+#     disp('Testing Small Values of A, B, and C:');
+#     testBealConjecture(A_range, B_range, C_range, x_range, y_range, z_range);
+#
+#     % Test 2: Large Values of A, B, and C
+#     disp('Testing Large Values of A, B, and C:');
+#     A_range_large = 10^8 : 10^9;   % Modify the range as needed
+#     B_range_large = 10^8 : 10^9;   % Modify the range as needed
+#     C_range_large = 10^8 : 10^9;   % Modify the range as needed
+#     testBealConjecture(A_range_large, B_range_large, C_range_large, x_range, y_range, z_range);
+#
+#     % Test 3: Primes and Prime Powers
+#     disp('Testing Primes and Prime Powers:');
+#     primes_range = primes(100);  % Modify the range as needed
+#     primes_powers_range = 3:9;   % Modify the range as needed
+#     testBealConjecture(primes_range, primes_range, primes_range, primes_powers_range, primes_powers_range, primes_powers_range);
+#
+#     % Test 4: Consecutive Numbers
+#     disp('Testing Consecutive Numbers:');
+#     consecutive_range = 1:10;   % Modify the range as needed
+#     testBealConjecture(consecutive_range, consecutive_range+1, consecutive_range+2, x_range, y_range, z_range);
+#
+#     % Test 5: Fibonacci Numbers
+#     disp('Testing Fibonacci Numbers:');
+#     fibonacci_range = generateFibonacciNumbers(20);  % Modify the range as needed
+#     testBealConjecture(fibonacci_range(2:1:end), fibonacci_range(2:1:end), fibonacci_range(2:1:end), x_range, y_range, z_range);
+#
+#     % Test 6: Special Exponents
+#     disp('Testing Special Exponents:');
+#     special_exponents_range = [42, 43, 44, 45];   % Modify the range as needed
+#     testBealConjecture(A_range, B_range, C_range, special_exponents_range, special_exponents_range, special_exponents_range);
+#
+#     % Test 7: Specific Diophantine Equations
+#     disp('Testing Specific Diophantine Equations:');
+#     testBealConjecture(A_range, B_range, C_range, x_range, 2*x_range, 3*x_range);
+#
+#
+# function testBealConjecture(A_range, B_range, C_range, x_range, y_range, z_range)
+#     % Loop through all combinations of A, B, C, x, y, and z
+#     for A = A_range
+#         for B = B_range
+#             for C = C_range
+#                 for x = x_range
+#                     for y = y_range
+#                         for z = z_range
+#                             % Check if A, B, and C do not share a common factor
+#                             if gcd(A, B) == 1 && gcd(A, C) == 1 && gcd(B, C) == 1
+#                                 % Use vpa to perform arithmetic with higher precision
+#                                 sum_powers = vpa(A)^x + vpa(B)^y;
+#                                 C_power = vpa(C)^z;
+#
+#                                 % Check if A^x + B^y = C^z
+#                                 if sum_powers == C_power
+#                                     % Report the values when the equation holds, and A, B, and C do not share a common factor
+#                                     disp(['Conjecture holds for A=', num2str(A), ...
+#                                         ', B=', num2str(B), ', C=', num2str(C), ...
+#                                         ', x=', num2str(x), ', y=', num2str(y), ', z=', num2str(z)]);
+#                                 end
+#                             end
+#                         end
+#                     end
+#                 end
+#             end
+#         end
+#     end
+# end
+#
+# function fibonacci_numbers = generateFibonacciNumbers(n)
+#     % Generate the first n Fibonacci numbers
+#     fibonacci_numbers = zeros(1, n);
+#     fibonacci_numbers(1) = 0;
+#     fibonacci_numbers(2) = 1;
+#     for i = 3:n
+#         fibonacci_numbers(i) = fibonacci_numbers(i-1) + fibonacci_numbers(i-2);
+#     end
+# end
+
+# ChatGPT
+# Sure, here's the Python version of the given MATLAB code for testing the Beal Conjecture:
+
 import math
+
 
 def gcd(a, b):
     while b != 0:
         a, b = b, a % b
     return a
+
 
 def testBealConjecture(A_range, B_range, C_range, x_range, y_range, z_range):
     for A in A_range:
@@ -19,11 +121,13 @@ def testBealConjecture(A_range, B_range, C_range, x_range, y_range, z_range):
                                 if sum_powers == C_power:
                                     print(f"Conjecture holds for A={A}, B={B}, C={C}, x={x}, y={y}, z={z}")
 
+
 def generateFibonacciNumbers(n):
     fibonacci_numbers = [0, 1]
     for i in range(2, n):
-        fibonacci_numbers.append(fibonacci_numbers[i-1] + fibonacci_numbers[i-2])
+        fibonacci_numbers.append(fibonacci_numbers[i - 1] + fibonacci_numbers[i - 2])
     return fibonacci_numbers
+
 
 # Define the range of values for A, B, C, x, y, and z
 A_range = range(1, 16)
@@ -49,12 +153,14 @@ testBealConjecture(A_range, B_range, C_range, x_range, y_range, z_range)
 print('Testing Primes and Prime Powers:')
 primes_range = list(filter(lambda x: all(x % d != 0 for d in range(2, int(math.sqrt(x)) + 1)), range(2, 101)))
 primes_powers_range = range(3, 10)
-testBealConjecture(primes_range, primes_range, primes_range, primes_powers_range, primes_powers_range, primes_powers_range)
+testBealConjecture(primes_range, primes_range, primes_range, primes_powers_range, primes_powers_range,
+                   primes_powers_range)
 
 # Test 4: Consecutive Numbers
 print('Testing Consecutive Numbers:')
 consecutive_range = range(1, 11)
-testBealConjecture(consecutive_range, [i+1 for i in consecutive_range], [i+2 for i in consecutive_range], x_range, y_range, z_range)
+testBealConjecture(consecutive_range, [i + 1 for i in consecutive_range], [i + 2 for i in consecutive_range], x_range,
+                   y_range, z_range)
 
 # Test 5: Fibonacci Numbers
 print('Testing Fibonacci Numbers:')
@@ -69,3 +175,54 @@ testBealConjecture(A_range, B_range, C_range, special_exponents_range, special_e
 # Test 7: Specific Diophantine Equations
 print('Testing Specific Diophantine Equations:')
 testBealConjecture(A_range, B_range, C_range, x_range, [2 * x for x in x_range], [3 * x for x in x_range])
+
+# Please note that this Python code will produce the same results as the MATLAB code provided. Also,
+# I've used list comprehensions and the filter function to generate prime numbers, as there is no built-in primes
+# function in Python like in MATLAB. The gcd function is manually implemented to calculate the greatest
+# common divisor of two numbers.
+
+# -> Bitte erkläre mir obiges Programm
+
+# ChatGPT
+#     Funktion:
+#     Diese Funktion führt die eigentlichen Tests für die Beal'sche Vermutung durch. Sie durchläuft alle möglichen
+#     Kombinationen von A, B, C, x, y und z und überprüft, ob die Bedingung A^x + B^y = C^z erfüllt ist,
+#     wenn A, B und C keinen gemeinsamen Teiler größer als 1 haben. Wenn die Bedingung erfüllt ist, werden die Werte
+#     von A, B, C, x, y und z ausgegeben, für die die Gleichung zutrifft.
+#
+#     generateFibonacciNumbers-Funktion:
+#     Diese Funktion generiert die ersten n Fibonacci-Zahlen. Sie wird verwendet, um die Testreihe
+#     mit Fibonacci-Zahlen durchzuführen.
+#
+# Nach der Definition der Funktionen folgen sieben Testfälle, die verschiedene Kombinationen von A, B, C, x, y und z
+# verwenden, um die Beal'sche Vermutung zu überprüfen. Die Testfälle umfassen verschiedene Wertebereiche für A, B, C
+# und Exponenten, einschließlich kleiner und großer Zahlen, Primzahlen, Fibonacci-Zahlen und spezielle Exponenten.
+#
+# Das Programm wird alle Kombinationen von A, B, C, x, y und z durchlaufen und für diejenigen, die
+# die Beal'sche Vermutung erfüllen, die entsprechenden Werte von A, B, C, x, y und z ausgeben.
+#
+# Bitte beachte, dass die Python-Version der Funktionen und Schleifen ähnlich zur MATLAB-Version ist,
+# aber es gibt einige Unterschiede in der Syntax und der Art und Weise, wie in Python mit Listen und Ganzzahlen
+# gearbeitet wird. Das Python-Programm erzeugt jedoch die gleichen Ergebnisse wie das ursprüngliche MATLAB-Programm.
+
+# -> Ist es korrekt, dass das Programm keine Ergebnisse liefert?
+
+# Ja, du hast recht. Es sieht so aus, als würde das Programm keine Ergebnisse liefern, wenn es mit den gegebenen
+# Eingabebereichen und Funktionen ausgeführt wird.
+#
+# Der Grund dafür liegt in der Bedingung, die in der Funktion testBealConjecture überprüft wird. Es wird geprüft,
+# ob A, B und C keinen gemeinsamen Teiler größer als 1 haben, und nur wenn diese Bedingung erfüllt ist,
+# wird die Gleichung A^x + B^y = C^z überprüft und mögliche Lösungen ausgegeben.
+#
+# Da die meisten Zahlen in den bereitgestellten Wertebereichen A_range, B_range und C_range nicht teilerfremd sind
+# (keinen gemeinsamen Teiler größer als 1 haben), wird die Bedingung für die Gleichung A^x + B^y = C^z
+# nur selten erfüllt. Dies führt dazu, dass das Programm keine oder nur sehr wenige Ergebnisse liefert.
+#
+# Wenn du das Programm mit anderen Wertebereichen oder spezifischen Zahlen ausprobieren möchtest, die möglicherweise
+# die Bedingung erfüllen, müsstest du die Eingabeparameter entsprechend anpassen. Zum Beispiel könntest du
+# kleinere Wertebereiche oder spezifische Zahlen auswählen, von denen du glaubst, dass sie die Beal'sche Vermutung
+# erfüllen könnten, um das Programm zu testen.
+#
+# Es ist jedoch wichtig zu beachten, dass die Beal'sche Vermutung bisher nicht bewiesen wurde, und es ist unbekannt,
+# ob es tatsächlich Zahlen gibt, die die Gleichung A^x + B^y = C^z ohne gemeinsamen Teiler größer als 1 erfüllen.
+# Die Suche nach solchen Zahlen ist Teil der ungelösten Probleme in der Zahlentheorie.
